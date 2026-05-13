319fb0feea9a41e58b18a3ddd50afd73f8daeda2
williamjeon7 <williamjeon7@kookmin.ac.kr>
2026-05-14 08:01:33 +0900

    fix: 프로필 이미지 업로드 처리 수정

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -15,6 +15,9 @@ apiClient.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+  if (config.data instanceof FormData) {
+    delete config.headers['Content-Type'];
+  }
   return config;
 });
 
